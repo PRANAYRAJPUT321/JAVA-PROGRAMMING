cfdba0a699bb4e4cb8c41a955675b1608c5817ec
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-02 22:12:39 +0530

    reverse star pattern

---
+++ ReverseStarpattern.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+ class ReverseStarpattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        for (int i = 5; i >= 1; i--)
+         {
+            for (int j = 1; j <= i; j++)
+             {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
