f64f1dd464a6fac0099f3a3c33319ba6789cbe7e
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-03 22:21:00 +0530

    changes in program

--- Foreach.java
+++ Foreach.java
@@ -1,10 +1,8 @@
-import java.util.Scanner;
-class Foreach {
+ class ForEachLoopExample {
     public static void main(String[] args) {
-      int marks[5]={60,70,80,90,80};
-        for (int i: marks)
-         {
-            System.out.println(marks[5]);
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int num : numbers) {
+            System.out.println(num);
         }
     }
 }
