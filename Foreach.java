28e44df9136a7ce01636634f668664816e47ec20
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-03 22:19:34 +0530

    for each loop example

---
+++ Foreach.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Foreach {
+    public static void main(String[] args) {
+      int marks[5]={60,70,80,90,80};
+        for (int i: marks)
+         {
+            System.out.println(marks[5]);
+        }
+    }
+}
