421d3c1faad6ab67a8b0ff7200f2f170cfbf66b5
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-03 22:36:29 +0530

    Another For Loop example

--- Forloop.java
+++ Forloop.java
@@ -1,13 +1,19 @@
 
 import java.util.Scanner;
 
- class Printingnumbers {
+ class Forloop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        for (int i = 1; i <= 10; i++)
+        for (int i = 1; i <= 4; i++)
          {
-            System.out.println(i);
+            for (int j = 1; j <= 4; j++)
+            {
+                System.out.println(i+" "+j);
+            }
+            
         }
+    
+       
     }
 }
