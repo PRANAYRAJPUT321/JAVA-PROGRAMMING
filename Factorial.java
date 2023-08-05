3244722310aab96767f303213b7f5c8584287061
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-05 20:17:28 +0530

    some required changes

--- Factorial.java
+++ Factorial.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 class Factorial{  
     public static void main(String args[]){  
      int i,fact=1;  
      Scanner sc = new Scanner(System.in);
      System.out.println("Enter the number to find Factorial");
-     int number = sc.nextInt();   
+     int number = sc.nextInt(i);   
      for(i=1;i<=number;i++){    
          fact=fact*i;    
      }    
