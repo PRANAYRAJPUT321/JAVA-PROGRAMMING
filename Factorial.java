adc4cdbbfa234134cd4e897bef26a8afe87ea6fa
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-05 20:15:24 +0530

    new filr created

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+class Factorial{  
+    public static void main(String args[]){  
+     int i,fact=1;  
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the number to find Factorial");
+     int number = sc.nextInt();   
+     for(i=1;i<=number;i++){    
+         fact=fact*i;    
+     }    
+     System.out.println("Factorial of "+number+" is: "+fact);    
+    }  
+   }
