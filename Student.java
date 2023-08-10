d046672937354282a95f1c182334892d5175390f
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-10 23:18:26 +0530

    simple program of objects and classes

---
+++ Student.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Student {
+    public static void main(String[] args) {
+        int id =10;
+        int No = 101;
+
+        
+    }
+}
---
+++ Student1.java
@@ -0,0 +1,9 @@
+class Student1{  
+    int id; 
+    String name;  
+    public static void main(String args[]){  
+     Student s1=new Student();
+     System.out.println(s1.id=110);
+     System.out.println(s1.name="rohan");  
+    }  
+   } 
