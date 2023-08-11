1113c24e6146203fba30301ae551963e2d33fff1
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-11 23:39:43 +0530

    Simple program for creating the constructor in java

--- Dynamite.java
+++ Dynamite.java
@@ -8,20 +8,21 @@ class Dynamite{
         n=name;
     }
     void display(){
-        System.out.println("i");
-        System.out.println("n");
+        System.out.println(id);
+        System.out.println(name);
     }
-}
-public static void main(String[] args) {
-    Dynamite Dy1 new= Dynamite(101,"Anil");
-    Dynamite Dy2 new= Dynamite(101,"Anil");
-    Dynamite Dy3 new= Dynamite(101,"Anil");
-    Dynamite Dy4 new= Dynamite(101,"Anil");
-    Dynamite Dd5 new= Dynamite(101,"Anil");
 
-    Dy1.display;
-    Dy2.display;
-    Dy3.display;
-    Dy4.display;
-    Dy5.display;
+public static void main(String[] args) {
+    Dynamite d1=new Dynamite();
+    Dynamite d2=new Dynamite();
+    Dynamite d3=new Dynamite();
+    Dynamite d4=new Dynamite();
+    Dynamite d5=new Dynamite();
+
+    d1.display();
+    d2.display();
+    d3.display();
+    d4.display();
+    d5.display();
+}
 }
