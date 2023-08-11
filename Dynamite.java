3e605dbd7aa6b2133a3f4edc0897e63a1590cd02
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-11 23:15:48 +0530

    cunstructor created

---
+++ Dynamite.java
@@ -0,0 +1,27 @@
+class Dynamite{
+    int id;
+    String name;
+
+    void Dynamite(int i,String n)
+    {
+        i=id;
+        n=name;
+    }
+    void display(){
+        System.out.println("i");
+        System.out.println("n");
+    }
+}
+public static void main(String[] args) {
+    Dynamite Dy1 new= Dynamite(101,"Anil");
+    Dynamite Dy2 new= Dynamite(101,"Anil");
+    Dynamite Dy3 new= Dynamite(101,"Anil");
+    Dynamite Dy4 new= Dynamite(101,"Anil");
+    Dynamite Dd5 new= Dynamite(101,"Anil");
+
+    Dy1.display;
+    Dy2.display;
+    Dy3.display;
+    Dy4.display;
+    Dy5.display;
+}
