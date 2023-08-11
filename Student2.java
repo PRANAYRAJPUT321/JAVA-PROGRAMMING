013060e4e5839bd0a031d0bf525df71660d32f16
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-11 22:59:53 +0530

    simple program of object and classes with fuctions

--- Student2.java
+++ Student2.java
@@ -1,4 +1,4 @@
-class Student{  
+class Student2{  
     int rollno;  
     String name;  
     void insertRecord(int r, String n){  
@@ -10,23 +10,27 @@ class Student{
         System.out.println(name);
     }  
    
-   }  
-   class TestStudent4{  
+  
+    
     public static void main(String args[]){  
    Student s1=new Student();  
    Student s2=new Student();  
    Student s3=new Student(); 
    Student s4=new Student(); 
+   Student s5=new Student(); 
+   Student s6=new Student(); 
      s1.insertRecord(101,"Pranay");  
      s2.insertRecord(102,"Pushpak");  
      s3.insertRecord(103,"Pratik");  
      s4.insertRecord(104,"Aryan");  
-     s4.insertRecord(105,"Ritik"); 
+     s5.insertRecord(105,"Ritik"); 
+     s6.insertRecord(106,"vikas"); 
      s1.displayInformation();  
      s2.displayInformation(); 
      s3.displayInformation();  
      s4.displayInformation(); 
      s5.displayInformation(); 
+     s6.displayInformation(); 
     }  
-   }  
+} 
    
