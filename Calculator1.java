6f59c25bdfeed7c50685590b790b58a5d1625846
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-15 23:40:48 +0530

    created more functions

--- Calculator1.java
+++ Calculator1.java
@@ -12,6 +12,27 @@ void Add (int a,int b){
 void DisplayAdd(){
     System.out.println("Addition of two number is : "+Add);
 }
+
+void Add (int a,int b){
+    Add=a+b;
+ }
+ void DisplayAdd(){
+     System.out.println("Addition of two number is : "+Add);
+ }
+
+ void Add (int a,int b){
+    Add=a+b;
+ }
+ void DisplayAdd(){
+     System.out.println("Addition of two number is : "+Add);
+ }
+
+ void Add (int a,int b){
+    Add=a+b;
+ }
+ void DisplayAdd(){
+     System.out.println("Addition of two number is : "+Add);
+ }
 public static void main(String[] args) {
     Calculator1 c1=new Calculator1();
 
