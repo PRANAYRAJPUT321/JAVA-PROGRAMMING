90dffc9eb0a713ae0463672d5e56de5e212c0ff1
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-15 23:52:20 +0530

    Airthmatic Operations

--- Calculator1.java
+++ Calculator1.java
@@ -9,35 +9,34 @@ class Calculator1{
 void Add (int a,int b){
    Add=a+b;
 }
-void DisplayAdd(){
+
+void Div (int a,int b){
+    Div=a/b;
+ }
+
+ void Mult (int a,int b){
+    Mult=a*b;
+ }
+
+ void Sub (int a,int b){
+    Sub=a-b;
+ }
+
+ void DisplayResult(){
     System.out.println("Addition of two number is : "+Add);
-}
-
-void Add (int a,int b){
-    Add=a+b;
- }
- void DisplayAdd(){
-     System.out.println("Addition of two number is : "+Add);
- }
-
- void Add (int a,int b){
-    Add=a+b;
- }
- void DisplayAdd(){
-     System.out.println("Addition of two number is : "+Add);
- }
-
- void Add (int a,int b){
-    Add=a+b;
- }
- void DisplayAdd(){
-     System.out.println("Addition of two number is : "+Add);
+    System.out.println("Division of two number is : "+Div);
+    System.out.println("Multiplication of two number is : "+Mult);
+     System.out.println("Substraction of two number is : "+Sub);
+    
  }
 public static void main(String[] args) {
     Calculator1 c1=new Calculator1();
 
     c1.Add(23,45);
+    c1.Div(20,5);
+    c1.Mult(23,45);
+    c1.Sub(23,45);
 
-    c1.DisplayAdd();
+    c1.DisplayResult();
 }
 }
