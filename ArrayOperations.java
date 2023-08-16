f569b440ebed3fa76899a528ad650278198ab0c2
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-16 12:32:34 +0530

    created array

---
+++ ArrayOperations.java
@@ -0,0 +1,28 @@
+class Array{
+    int []aar1;
+    int []aar2;
+    int []arr3;
+
+    void InsertArray(){
+        aar1=new int[] {1,2,3,4,5};
+        arr2=new int[arr1.lenth]
+    }
+
+    void CopyArray(){
+        for(i=0;i<arr1.length){
+            arr2[i]=arr1[i]
+        }
+    }
+
+    void DispalyArray(){
+        for(i=0; i<arr1.length ){
+            System.out.println(arr1 +" ");
+        }
+        for(i=0; i<arr2.length ){
+            System.out.println(arr2 +" ");
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
