0ebf64d4c56d0a98953a74172609a22cff731039
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-20 22:05:34 +0530

    functions created

--- All  JAVA PROGRAMS/IncomeTax.java
+++ All  JAVA PROGRAMS/IncomeTax.java
@@ -2,4 +2,16 @@ class IncomeTax{
     int Income;
     int Tax;
 
+    void InsertIncome(int i){
+    Income=i;
+    }
+
+    void Taxes(){
+        Tax=Income*28/100;
+    }
+    void DisplayTotalTax(){
+        System.out.println("Tax to pay is"+Tax);
+    }
+    
+
 }
