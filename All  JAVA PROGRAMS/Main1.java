f84b60c88e3a3207ee7566ee83f6040f383980e1
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-20 22:45:24 +0530

    Java program to check whether the entered number is Amstrong or not using constructor

---
+++ All  JAVA PROGRAMS/Main1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        
+        Armstrong checker = new Armstrong(num);
+        
+        if (checker.isArmstrong()) {
+            System.out.println(num + " is an Armstrong number.");
+        } else {
+            System.out.println(num + " is not an Armstrong number.");
+        }
+        
+        scanner.close();
+    }
+}
