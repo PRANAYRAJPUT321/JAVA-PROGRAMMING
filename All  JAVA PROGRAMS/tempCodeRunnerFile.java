237b77ba7245b89d5baef73579a0f257e596af56
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-22 23:22:07 +0530

    java program for object initialisation through reference

--- All  JAVA PROGRAMS/tempCodeRunnerFile.java
+++ All  JAVA PROGRAMS/tempCodeRunnerFile.java
@@ -1,14 +1,2 @@
-class Object 
- { 
- void show() 
- { 
-     System.out.println("Welcome to java Programming"); 
- } 
- public static void main(String[] args) 
- { 
- Object obj = new Object(); 
-
- obj.show(); 
- 
- } 
- }
+} 
+    //  class TestStudent{
