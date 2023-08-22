7cc0fd92c98e3f9866083b08fd0814ea6687ef10
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-22 22:47:24 +0530

    java program to reverse a number

--- All  JAVA PROGRAMS/tempCodeRunnerFile.java
+++ All  JAVA PROGRAMS/tempCodeRunnerFile.java
@@ -1,1 +1,5 @@
-interest=(a*r*t)/100;
+System.out.println("Elements of Third array :");
+        // for(int i=0; i<arr1.length; i++)
+        // {
+        //     System.out.println(arr3[i] + " ");
+        // }
