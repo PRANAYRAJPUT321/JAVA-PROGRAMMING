809145ef5ec70ec1d86f40b26fa81c25d017e83a
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-22 23:40:07 +0530

    object initialisation through method

--- All  JAVA PROGRAMS/Student1.java
+++ All  JAVA PROGRAMS/Student1.java
@@ -1,9 +1,27 @@
-class Student1{  
-    int id; 
-    String name;  
-    public static void main(String args[]){  
-     Student s1=new Student();
-     System.out.println(s1.id=110);
-     System.out.println(s1.name="rohan");  
-    }  
-   } 
+ class Student1{ 
+     int rollno; 
+     String name; 
+
+     void insertRecord(int r, String n){ 
+     rollno=r; 
+     name=n; 
+     } 
+     void displayInformation()
+     {
+        System.out.println(rollno+" "+name);
+    } 
+     
+     public static void main(String args[]){ 
+     Student1 s1=new Student1(); 
+     Student1 s2=new Student1(); 
+     Student1 s3=new Student1(); 
+
+     s1.insertRecord(101,"PRANAY"); 
+     s2.insertRecord(102,"PRAKSH"); 
+     s3.insertRecord(103,"MUKESH");
+
+     s1.displayInformation(); 
+     s2.displayInformation(); 
+     s3.displayInformation(); 
+     } 
+     } 
