725ecb74b5e553dcbe4c4a77ca53a9095be6f559
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-23 23:41:39 +0530

    added comments in programs

--- All  JAVA PROGRAMS/Add.java
+++ All  JAVA PROGRAMS/Add.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
         System.out.print("Enter the second number: ");
         int num2 = scanner.nextInt();
-
+       
         int sum = num1 + num2;
         System.out.println("The sum of the two numbers is: " + sum);
         scanner.close();
