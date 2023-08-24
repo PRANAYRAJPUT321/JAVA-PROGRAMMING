a27ea9e4a21c3c7de64984bb8348114eda455f45
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-08-24 22:24:33 +0530

    functions are created

--- All  JAVA PROGRAMS/student3.java
+++ All  JAVA PROGRAMS/student3.java
@@ -3,6 +3,15 @@ class Student3{
     int rollno;
     String branch;
     String collage;
-    
+
+    void insertdata (String n,int r,String b,String c){
+        name =n;
+        rollno=r;
+        branch=b;
+        collage=c;
+    }
+    void dispalydata(){
+        System.out.println("Name of Students is"+name+"Roll no = "+rollno+"Branch is :"+branch+"Collage name is : "+collage);
+    }
 
 }
