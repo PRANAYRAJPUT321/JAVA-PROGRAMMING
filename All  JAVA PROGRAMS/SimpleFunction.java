322d178559be072bada0b7a969c6ed263dde180c
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-09-04 23:51:28 +0530

    some changes

--- All  JAVA PROGRAMS/SimpleFunction.java
+++ All  JAVA PROGRAMS/SimpleFunction.java
@@ -1,9 +1,21 @@
 class SimpleFunction {
     int a ;
     int b;
+    int c;
 
     void insert (int a,int b){
-        
+        a=a;
+        b=b;
     }
 
+    void mult(){
+        c=a*b;
+    }
+
+    void display(){
+        System.out.println(c);
+    }
+public static void main(String[] args) {
+    
+}
 }
