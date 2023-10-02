7134692e8a2d0e243ec65de0e8531f852ed2f308
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-10-02 22:05:22 +0530

    new program added

--- All  JAVA PROGRAMS/Teststring1.java
+++ All  JAVA PROGRAMS/Teststring1.java
@@ -1,3 +1,6 @@
-public class Teststring1 {
-    
-}
+public class Teststring1{  
+public static void main(String args[]){  
+String name="javatpoint";  
+char ch=name.charAt(4);
+System.out.println(ch);  
+}} 
