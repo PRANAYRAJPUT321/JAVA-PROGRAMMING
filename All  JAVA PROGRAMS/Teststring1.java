8c2f48d0ec5aa8f7a6880b1f5a4d57553414ff65
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2023-10-03 10:42:48 +0530

    New Program Added

---
+++ All  JAVA PROGRAMS/Teststring1.java
@@ -0,0 +1,16 @@
+public class Teststring1{  
+public static void main(String args[]){  
+String name=" My name is Pranay";  
+char ch=name.charAt(6);
+int index=name.indexOf('s');
+String namelower = name.toLowerCase();
+String nameupper = name.toUpperCase();
+
+System.out.println("Character at perticular index is :-  " +ch); 
+System.out.println("string length is:- "+name.length());
+System.out.println("Checking Whether String is Empty :- " + name.isEmpty()); 
+System.out.println("Index of Particular character is :- " +index);
+System.out.println("String in Lower Case :- " + namelower);
+System.out.println("String in Upper Case :- " +nameupper);
+System.out.println("Trimmed String :- " +name.trim());
+}} 
